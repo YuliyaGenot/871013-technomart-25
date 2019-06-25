4d507dde4a938ac9bfa0f2b5e11c357647fd080b
YuliyaGenot <50360821+YuliyaGenot@users.noreply.github.com>
2019-06-25 20:44:43 +0300

    Защита проекта 2 попытка

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@
   var mapPopup = document.querySelector(".modal-map");
   var mapClose = mapPopup.querySelector(".modal-close");
 
+  var feedback = document.querySelector(".feedback-write");
+  var feedbackPopup = document.querySelector(".feedback");
+  var feedbackClose = feedbackPopup.querySelector(".modal-close");
+
+
+  var buy = document.querySelector(".in-stock");
+  var buyPopup = document.querySelector(".modal-buy");
+  var buyClose = buyPopup.querySelector(".modal-close");
+
+
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
@@ -36,6 +46,47 @@ window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+
+feedback.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.add("modal-show");
+ });
+
+ feedbackClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove("modal-show");
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (feedbackPopup.classList.contains("modal-show")) {
+      feedbackPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+buy.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buyPopup.classList.add("modal-show");
+ });
+
+ buyClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buyPopup.classList.remove("modal-show");
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (buyPopup.classList.contains("modal-show")) {
+      buyPopup.classList.remove("modal-show");
     }
   }
 });
